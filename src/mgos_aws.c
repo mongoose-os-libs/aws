8ebf39877c6a520b04c113dc68c1c6f82df304d1
Dmitry Frank <mail@dmitryfrank.com>
2017-12-20 12:42:02 +0200

    Persist expanded MAC placeholders in thing_name
    PUBLISHED_FROM=ccdf2d2c20f093fed120820e9221657cd4dfff53

--- src/mgos_aws.c
+++ src/mgos_aws.c
@@ -19,6 +19,16 @@ const char *mgos_aws_get_thing_name(void) {
 }
 
 bool mgos_aws_init(void) {
+  /*
+   * If thing_name is set explicitly, persist expanded MAC placeholders in it
+   */
+  if (mgos_sys_config_get_aws_thing_name() != NULL) {
+    char *thing_name = strdup(mgos_sys_config_get_aws_thing_name());
+    mgos_expand_mac_address_placeholders(thing_name);
+    mgos_sys_config_set_aws_thing_name(thing_name);
+    free(thing_name);
+  }
+
   LOG(LL_INFO, ("AWS Greengrass enable (%d)",
                 mgos_sys_config_get_aws_greengrass_enable()));
 
